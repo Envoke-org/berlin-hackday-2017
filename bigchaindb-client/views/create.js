fc713c81e45303e0121a90e0b77344a17d778e73
Augustin Godiscal <dev@auggod.io>
2017-07-11 17:09:54 +0200

    Use file reader

--- bigchaindb-client/views/create.js
+++ bigchaindb-client/views/create.js
@@ -4,12 +4,33 @@ module.exports = function (state, emit) {
   return html`
     <div class="container">
       <form>
-        <input ondrop=${drop} type="file" id="file" name="file" />
+        <input onchange=${onChange} type="file" id="files" />
       </form>
     </div>
   `
 
-  function drop (e) {
-    console.log(e)
+  function onChange (e) {
+    e.preventDefault()
+    e.stopPropagation()
+    const files = e.target.files[0]
+    const types = !((/audio\/mpeg|audio\/mp3|audio\/mp4|audio\/ogg|audio\/x+|wav/).test(files.type))
+
+    if (types) {
+      window.alert('no audio file')
+      return
+    }
+
+    var reader = new window.FileReader()
+
+    console.log(reader)
+
+    reader.onload = (e) => {
+      const music = e.target.result
+      // see link
+      var audio = new window.Audio(music)
+      audio.play()
+    }
+    // substitute `readAsDataURL` for `readAsArrayBuffer`
+    reader.readAsDataURL(files)
   }
 }
