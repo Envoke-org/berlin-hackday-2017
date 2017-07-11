133a6aebd0d65f6243168f729f90023ff23ba505
Augustin Godiscal <dev@auggod.io>
2017-07-11 17:01:13 +0200

    Add file input

--- bigchaindb-client/views/create.js
+++ bigchaindb-client/views/create.js
@@ -3,7 +3,13 @@ const html = require('choo/html')
 module.exports = function (state, emit) {
   return html`
     <div class="container">
-      <h1>Create</h1>
+      <form>
+        <input ondrop=${drop} type="file" id="file" name="file" />
+      </form>
     </div>
   `
+
+  function drop (e) {
+    console.log(e)
+  }
 }
