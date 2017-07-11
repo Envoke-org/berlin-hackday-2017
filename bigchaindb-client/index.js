3bc334762c905a93cb45cf498da2496a3096f05f
Augustin Godiscal <dev@auggod.io>
2017-07-11 15:45:54 +0200

    Setting up form + bootstrap

--- bigchaindb-client/index.js
+++ bigchaindb-client/index.js
@@ -1,51 +1,36 @@
 const envoke = require('./lib/index.js')
 const driver = require('bigchaindb-driver')
-const html = require('choo/html')
 const log = require('choo-log')
 const choo = require('choo')
 const extend = require('xtend')
+const css = require('sheetify')
+
+css('./styles/bootstrap.min.css')
 
 // BigchainDB server instance or IPDB (e.g. https://test.ipdb.io/api/v1/)
 const API_PATH = 'http://localhost:9984/api/v1/'
 
-const { schema } = envoke
+const { newPerson, newOrganization, newMusicGroup } = envoke.schema
 
 const app = choo()
 app.use(log())
 app.use(store)
-app.route('/', mainView)
+app.route('/', require('./views/main'))
 app.mount('body')
 
-function mainView (state, emit) {
-  function handleSubmit(e) {
-    e.preventDefault()
-    emit('create-user', {
-      birthDate : e.target.birthDate.value,
-      familyName : e.target.familyName.value,
-      givenName : e.target.givenName.value,
-      image: e.target.image.value
-    })
-  }
-  return html`
-    <body>
-      ${state.user}
-      <form onsubmit=${handleSubmit}>
-        <input type="text" name="birthDate" placeholder="Birthdate: MM/DD/YYYY" value=${state.form.birthDate} />
-        <input type="text" name="familyName" placeholder="Family name" value=${state.form.familyName} />
-        <input type="text" name="givenName" placeholder="Given name" value=${state.form.givenName} />
-        <input type="text" name="image" placeholder="Image url" value=${state.form.image} />
-        <button type="submit">Register</button>
-      </form>
-    </body>
-  `
-}
-
 function store (state, emitter) {
-  state.form = {
-    birthDate: '',
-    familyName: '',
-    givenName: '',
-    image: ''
+  if (!state.form) {
+    state.form = {
+      birthDate: '',
+      familyName: '',
+      givenName: '',
+      image: '',
+      name: '',
+      description: '',
+      email: '',
+      members: '',
+      type: 'Person'
+    }
   }
 
   if (!state.key) {
@@ -57,15 +42,41 @@ function store (state, emitter) {
     emitter.on('create-key-pair', createKeyPair)
     emitter.on('create-transaction', createTransaction)
     emitter.on('create-user', createUser)
+    emitter.on('update-type', (value) => {
+      state.form.type = value
+      emitter.emit('render')
+    })
   })
 
   function createUser (payload) {
-    const { birthDate, familyName, givenName, image } = payload
-    const user = schema.newPerson(birthDate, familyName, givenName, image)
+    const {
+      type = 'Person',
+      birthDate = '',
+      familyName = '',
+      givenName = '',
+      image = '',
+      name = '',
+      description = '',
+      email = '',
+      members = ''
+    } = payload
+
+    function setUser () {
+      switch (type) {
+        case 'Person':
+          return newPerson(birthDate, familyName, givenName, image)
+        case 'Organization':
+          return newOrganization(name, description, email, members, image)
+        case 'MusicGroup':
+          return newMusicGroup(name, description, email, members, image)
+        default:
+      }
+    }
+
+    const user = setUser()
 
     state.form = payload
     state.user = user
-    console.log(user, payload)
     emitter.emit('render')
   }
 
@@ -73,7 +84,6 @@ function store (state, emitter) {
   function createKeyPair () {
     const key = new driver.Ed25519Keypair()
     state.key = key
-    console.log(key)
     emitter.emit('render')
   }
 
---
+++ bigchaindb-client/views/main.js
@@ -0,0 +1,90 @@
+const html = require('choo/html')
+const extend = require('xtend')
+
+const options = ['Person', 'Organization', 'MusicGroup'].map((n) => ({ value: n, label: n }))
+
+module.exports = function (state, emit) {
+  function select (options, value = state.form.type) {
+    return html`
+      <select onchange=${(e) => emit('update-type', e.target.value)} name="type">
+        ${options.map((o) =>
+          html`<option value="${o.value}"
+              selected="${value === o.value}">
+              ${o.label}
+          </option>`
+        )}
+      </select>`
+  }
+  function handleSubmit (e) {
+    e.preventDefault()
+    const type = e.target.type.value
+    const person = {
+      birthDate: e.target.birthDate.value,
+      familyName: e.target.familyName.value,
+      givenName: e.target.givenName.value,
+      image: e.target.image.value
+    }
+    const other = {
+      name: e.target.name.value,
+      description: e.target.description.value,
+      email: e.target.email.value,
+      members: e.target.members.value,
+      image: e.target.image.value
+    }
+    emit('create-user', extend(type === 'Person' ? person : other, { type }))
+  }
+  return html`
+    <body>
+      <div class="container">
+        <div class="row">
+          <form onsubmit=${handleSubmit}>
+            ${select(options)}
+            ${state.form.type === 'Person' ? PersonForm() : DefaultForm()}
+            <button class="btn btn-default" type="submit">Register</button>
+          </form>
+        </div>
+      </div>
+    </body>
+  `
+
+  function DefaultForm () {
+    return html`
+      <fieldset>
+        <div class="form-group">
+          <input class="form-control" type="text" name="name" placeholder="Name" value=${state.form.name} required />
+        </div>
+        <div class="form-group">
+          <input class="form-control" type="text" name="description" placeholder="Description" value=${state.form.description} required />
+        </div>
+        <div class="form-group">
+          <input type="email" class="form-control" name="email" placeholder="Email" value=${state.form.email} required />
+        </div>
+        <div class="form-group">
+          <input type="text" class="form-control" name="members" placeholder="Members" value=${state.form.members} required />
+        </div>
+        <div class="form-group">
+          <input class="form-control" type="text" name="image" placeholder="Image url" value=${state.form.image} required />
+        </div>
+      </fieldset>
+    `
+  }
+
+  function PersonForm () {
+    return html`
+      <fieldset>
+        <div class="form-group">
+          <input class="form-control" type="text" name="birthDate" placeholder="Birthdate: MM/DD/YYYY" value=${state.form.birthDate} required />
+        </div>
+        <div class="form-group">
+          <input class="form-control" type="text" name="familyName" placeholder="Family name" value=${state.form.familyName} required />
+        </div>
+        <div class="form-group">
+          <input class="form-control" type="text" name="givenName" placeholder="Given name" value=${state.form.givenName} required />
+        </div>
+        <div class="form-group">
+          <input class="form-control" type="text" name="image" placeholder="Image url" value=${state.form.image} required />
+        </div>
+      </fieldset>
+    `
+  }
+}
